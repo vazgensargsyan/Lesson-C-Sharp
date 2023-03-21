d259a19eff8c342f43d8701f608ec4b1a042925c
Vazgen Sargsyan <vazgensargsyan2@gmail.com>
2023-03-21 21:48:56 +0400

    OOP 7

---
+++ OOP_7/GenericClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_7
+{
+    internal class GenericClass<T>
+    {
+        public T Value { get; set; }
+        public T IsString(T myString, ref bool isString)
+        {
+            Value = myString;
+            if (myString is string)
+            {
+                isString = true;
+            }
+            return Value;
+        }
+    }
+}
---
+++ OOP_7/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace OOP_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericClass<string> genericClass = new GenericClass<string>();
+            bool isString = false;
+            Console.WriteLine(genericClass.IsString("valodik", ref isString));
+            Console.WriteLine(isString);
+        }
+    }
+}
